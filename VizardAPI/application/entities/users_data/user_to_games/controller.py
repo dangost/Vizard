d669206a4a882e9de26269bb479126ab17439bfd
Dan Gost <dangost16@gmail.com>
2020-10-14 22:50:34 +0300

    controller for utg

--- VizardAPI/application/entities/users_data/user_to_games/controller.py
+++ VizardAPI/application/entities/users_data/user_to_games/controller.py
@@ -1,5 +1,43 @@
-# controller
+from flask import Blueprint, jsonify, request
+from .repository import UserToGamesRepository
+from .schema import UserToGamesSchema
 
-# GET POST PUT DELETE
+studios_controller_api = Blueprint('studios_controller_api', __name__)
 
-##
+rep = UserToGamesRepository()
+
+
+@studios_controller_api.route("/api/Studios/" or "/api/Studios", methods=['GET'])
+def get_all():
+    data = rep.get_all_utd()
+    return jsonify(UserToGamesRepository(many=True).dump(data))
+
+
+@studios_controller_api.route("/api/Studios/<int:studio_id>/" or "/api/Studios/<int:studio_id>", methods=['GET'])
+def get_id(studio_id):
+    obj = rep.get_id_utd(studio_id)
+    if type(obj) is str:
+        return obj
+    return jsonify(UserToGamesRepository(many=False).dump(obj))
+
+
+@studios_controller_api.route("/api/Studios/" or "/api/Studios", methods=['POST'])
+def post():
+    json_data = request.get_json()
+    result = rep.post_utd(json_data)
+    return result
+
+
+@studios_controller_api.route("/api/Studios/<int:studio_id>/" or "/api/Studios/<int:studio_id>", methods=['PUT'])
+def put(studio_id):
+    json_data = request.get_json()
+    result = rep.put_utd(studio_id, json_data)
+    return result
+
+
+@studios_controller_api.route("/api/Studios/<int:studio_id>/" or "/api/Studios/<int:studio_id>", methods=['DELETE'])
+def delete(studio_id):
+    result = rep.delete_utd(studio_id)
+    return result
+
+
